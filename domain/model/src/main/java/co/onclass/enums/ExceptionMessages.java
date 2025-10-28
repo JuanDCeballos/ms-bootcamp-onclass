75031aebad2f2e71a9e20c283671a5a1d6cea683
Juan Ceballos <juandavi789@hotmail.com>
2025-10-27 19:21:36 -0500

    Added new messages to the enum of exceptions messages

--- domain/model/src/main/java/co/onclass/enums/ExceptionMessages.java
+++ domain/model/src/main/java/co/onclass/enums/ExceptionMessages.java
@@ -7,7 +7,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum ExceptionMessages {
 
-    PRUEBA(0, "PRUEBA");
+    BOOTCAMP_NO_ENCONTRADO(404, "El Bootcamp no fue encontrado."),
+    BOOTCAMP_CON_CAPACIDADES_DUPLICADAS(400, "El Bootcamp no permite capacidades duplicadas."),
+    MONTO_CAPACIDADES_A_ASIGNAR(400, "El Bootcamp permite entre 1 y 4 capacidades."),
+    ERROR_DE_NEGOCIO_EN_MICROSERVICIO_EXTERNO(400, "Error de negocio en microservicio externo."),
+    ERROR_TECNICO_EN_MICROSERVICIO_EXTERNO(500, "Error técnico en microservicio externo."),
+    ;
 
     private final int code;
     private final String message;
