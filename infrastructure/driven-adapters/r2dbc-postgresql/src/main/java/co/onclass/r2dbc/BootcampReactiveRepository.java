92879b116c6dabdf1e38a64dc937b42194f0f8ea
Juan Ceballos <juandavi789@hotmail.com>
2025-10-28 23:24:37 -0500

    Added all db repositories logic for  getting the info paginated

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/onclass/r2dbc/BootcampReactiveRepository.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/onclass/r2dbc/BootcampReactiveRepository.java
@@ -1,10 +1,14 @@
 package co.onclass.r2dbc;
 
 import co.onclass.r2dbc.entity.BootcampEntity;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface BootcampReactiveRepository
         extends ReactiveCrudRepository<BootcampEntity, Long>, ReactiveQueryByExampleExecutor<BootcampEntity> {
 
+    @Query("SELECT COUNT(*) FROM bootcamps")
+    Mono<Long> countAll();
 }
